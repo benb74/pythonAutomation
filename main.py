25500353344843f0cca37dcec1752d9a8a7e4c40
benoit <bbequignon@gmail.com>
2021-12-09 16:01:53 +0100

    add list products with inventory less than 10

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ product_list = inv_file["Sheet1"]
 
 products_per_supplier = {}
 total_value_per_supplier = {}
+product_under_25_inv = {}
 
 for product_row in range(2,
                          product_list.max_row + 1):  # range(2, max_row + 1)  is [2,3, 4, ..., last row] (row 1 is columns title and don't have to be treated and max_row is total row -1)
     supplier_name = product_list.cell(product_row, 4).value  # each cell for the column 4 Suppliers
     inventory = product_list.cell(product_row, 2).value
     price = product_list.cell(product_row, 3).value
+    product_number = product_list.cell(product_row, 1).value
 
     # calculate how products per supplier
     if supplier_name in products_per_supplier:
@@ -27,5 +29,8 @@ for product_row in range(2,
     else:
         total_value_per_supplier[supplier_name] = inventory * price
 
-print(products_per_supplier)
-print(total_value_per_supplier)
+    # calculate logic product inventory less than 25
+    if inventory < 25:
+        product_under_25_inv[product_number] = int(inventory)
+
+print(product_under_25_inv)
